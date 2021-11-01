339d8e32e2eede47fa2bfd14872b2a3175666cd5
marcusvfgaldino <mvgaldino60@gmail.com>
2021-11-01 20:58:21 -0300

    Adding application routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+
 
 export default () => {
   return (
-    <div>
-        Olá Mundo!
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
