2f1e193a52082d4db559209db0ace75c9a8a0b42
marcusvfgaldino <mvgaldino60@gmail.com>
2021-11-03 17:22:17 -0300

    Insertion title and logo in login page

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LoginButton from "../../components/LoginButton";
 import LoginForm from "../../components/LoginForm";
+import CalendarLogo from "../../logo.png";
 
 const Login = () => {
 
@@ -10,6 +11,10 @@ const Login = () => {
 
     return (
         <section className="login__section">
+            <h1>Minha Agenda Minha Vida</h1>
+            <div className="login__logo">
+                <img alt="Logo" src={CalendarLogo} />
+            </div>
             <div className="box__login">
                 <LoginForm />
                 <LoginButton />
